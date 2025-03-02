f7f413d39719f8c69fdf75ffcb9b987c1b3e7ec4
ravi <rv200414z@gmail.com>
2025-03-02 23:55:28 +0530

    url redirect work

--- controller/urlshortner.controller.ts
+++ controller/urlshortner.controller.ts
@@ -2,11 +2,16 @@ import { Request, Response, NextFunction } from "express";
 import logger from "../utils/errorlogs";
 import { CheckCustomName, isValidURL } from "../utils/helper";
 import ExecuteQuery from "../config/db";
+import dotenv from "dotenv";
+dotenv.config();
 
 interface AuthenticatedRequest extends Request {
-    user?: unknown;
+    user?: {
+        id?: string;
+    }
   }
 
+// Controller to Shorten the URL
 export const shortUrl = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response | void> => {
     try {
         let { originalUrl, customName } = req.body;
@@ -25,20 +30,54 @@ export const shortUrl = async (req: AuthenticatedRequest, res: Response, next: N
             return res.status(400).json({ msg: "Invalid Custom Name" });
         }
 
+        let userId = req?.user?.id// Get the User ID from the Request
+        console.log(userId);
+
         // Check if the custom name already exists
         let isNameExists = await ExecuteQuery(
-            "SELECT customname FROM url_shortener WHERE customname = $1", 
-            [customName]
+            "SELECT customname FROM urls WHERE user_id = $1", 
+            [userId]
         );
-        if(isNameExists.rowCount > 0) {
+        if(isNameExists.length > 0) {
             return res.status(400).json({ msg: "Custom Name Already Exists" });
         }
 
+        const shortUrl = `${process.env.BASE_URL}/${customName}`;
+        await ExecuteQuery(
+          "INSERT INTO urls (original_url, customname, short_url, user_id) VALUES ($1, $2, $3, $4)",
+          [originalUrl, customName, shortUrl, userId]
+        );
 
-        let userId = req?.user// Get the User ID from the Request
+        return res.status(201).json({success: true, shortUrl });
+
 
     } catch (error) {
         logger.error(error);
+        return res.status(500).json({ msg: "Internal Server Error", error });
+    }
+}
+
+// Controller to Redirect the Shortened URL
+export const redirectUrl = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+        let {  customName } = req.params;
+        console.log(customName);
+
+        if(!customName) {
+            return res.status(400).json({ msg: "Invalid URL" });
+        }
+
+        let url = await ExecuteQuery(
+            "SELECT original_url FROM urls WHERE customname = $1",
+            [customName]
+        );
+
+        if(url.length === 0) {
+            return res.status(404).json({ msg: "URL Not Found" });
+        }
+        res.redirect(url[0].original_url);
+    } catch (error) {
+        console.error(error);
         return res.status(500).json({ msg: "Internal Server Error" });
     }
 }
--- routes/urlroute.ts
+++ routes/urlroute.ts
@@ -1,8 +1,12 @@
 import { Router, Request, Response, NextFunction } from "express";
 const route = Router();
 import { shortUrl } from "../controller/urlshortner.controller";
+import { Authmiddleware } from "../middlewares/authmiddleware"
 
 // Endpoint to handle the URL request
-route.post("/shorturl", shortUrl as (req: Request, res: Response, next: NextFunction) => Promise<void>);
+route.post("/shorturl", Authmiddleware, shortUrl as (req: Request, res: Response, next: NextFunction) => Promise<void>);
+
+// Endpoint to handle the Redirect URL
+route.get("/:customName", Authmiddleware, shortUrl as (req: Request, res: Response, next: NextFunction) => Promise<void>);
 
 export default route;
